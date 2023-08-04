4028dc396ea5d17f7be7cee2566facb85a25671a
Arkadiusz Galperyn <agalperin@9bits.pl>
2023-08-04 12:09:21 +0200

    If no drawable resource attached to item's name then collins glass by default.

--- app/src/main/java/com/example/coctailcard/ui/glassscreen/GlassSingleCard.kt
+++ app/src/main/java/com/example/coctailcard/ui/glassscreen/GlassSingleCard.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,12 +15,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.example.coctailcard.R
 import com.example.coctailcard.data.network.models.Glass
 import com.example.coctailcard.ui.theme.Black1
@@ -47,12 +44,14 @@ fun GlassSingleCard(
                 .fillMaxHeight()
                 .fillMaxWidth(0.45f)
                 .align(Alignment.BottomStart),
-            painter = painterResource(id = R.drawable.balloon_glass),
+            painter = painterResource(id = glass.imageRes ?: R.drawable.balloon_glass),
             contentDescription = "Cocktail detail card",
             contentScale = ContentScale.Fit
         )
         Column(
-            modifier = Modifier.align(Alignment.TopCenter).padding(start = 32.dp, top = 8.dp)
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(start = 32.dp, top = 8.dp)
         ) {
             Text(text = glass.name)
             glass.description?.let { Text(text = it) }
