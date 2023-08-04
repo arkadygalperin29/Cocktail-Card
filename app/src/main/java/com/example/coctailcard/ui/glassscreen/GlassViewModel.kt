3b9112271cf9d7616c3c2bed0af6ea916c47258f
Arkadiusz Galperyn <agalperin@9bits.pl>
2023-08-04 11:49:59 +0200

    GlassViewModel fetch function for glass names.

--- app/src/main/java/com/example/coctailcard/ui/glassscreen/GlassViewModel.kt
+++ app/src/main/java/com/example/coctailcard/ui/glassscreen/GlassViewModel.kt
@@ -1,7 +1,31 @@
 package com.example.coctailcard.ui.glassscreen
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.coctailcard.data.network.RequestResult
+import com.example.coctailcard.data.network.models.Cocktail
+import com.example.coctailcard.data.network.models.Glass
 import com.example.coctailcard.data.repositories.glasses.GetGlassesRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class GlassViewModel(private val glassesRepository: GetGlassesRepository) : ViewModel() {
+
+    private val _glasses = MutableStateFlow<List<Glass>>(emptyList())
+    val glasses = _glasses.asStateFlow()
+
+    suspend fun fetchGlasses() {
+        when (val result = glassesRepository.getGlassesList()) {
+            is RequestResult.Success -> {
+                runCatching {
+                    _glasses.value = result.data
+                    Log.d("CocktailReq", "${result.data}")
+                }.onFailure {
+                    it.printStackTrace()
+                }
+            }
+            else -> {
+            }
+        }
+    }
 }
